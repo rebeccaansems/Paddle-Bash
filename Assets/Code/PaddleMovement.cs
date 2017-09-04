774bc51e5460c3a8d2215b918b517f5f3f9385b2
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-09-04 20:17:13 -0300

    Added walls and collisions

--- Assets/Code/PaddleMovement.cs
+++ Assets/Code/PaddleMovement.cs
@@ -7,19 +7,48 @@ public class PaddleMovement : MonoBehaviour
     public float playerSpeed;
     public float rotateSpeed;
 
+    private bool canMove;
+
     void FixedUpdate()
     {
-        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
-
-        if (Input.GetKey(KeyCode.Q))
+        if(Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
-            transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * playerSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
 
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
-            transform.Rotate(Vector3.back * rotateSpeed * Time.deltaTime);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, Input.GetAxisRaw("Vertical") * playerSpeed);
+        }
+
+        if(Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, 0);
+        }
+
+
+
+        if (Input.GetAxisRaw("Rotate") > 0.5f || Input.GetAxisRaw("Rotate") < -0.5f)
+        {
+            transform.Rotate(Vector3.forward * rotateSpeed * Input.GetAxisRaw("Rotate") * Time.deltaTime);
+        }
+
+        //if (Input.GetKey(KeyCode.E))
+        //{
+        //    transform.Rotate(Vector3.back * rotateSpeed * Time.deltaTime);
+        //}
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "prop_powerCube")
+        {
+            Destroy(col.gameObject);
         }
     }
 }
