910eecd4d8e46768c622e8fcd17e750ab87cd2ec
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-09-04 19:16:06 -0300

    Setup basic movement

---
+++ Assets/Code/PaddleMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour
+{
+    public float playerSpeed;
+    public float rotateSpeed;
+
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        GetComponent<Rigidbody2D>().velocity = targetVelocity * playerSpeed;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(Vector3.back * rotateSpeed * Time.deltaTime);
+        }
+    }
+}
