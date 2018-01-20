0027ed18fe4c89a064f45348556fc6dc0c0c7848
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-20 15:58:22 -0400

    Added finish zone

--- Assets/Code/BallMovement.cs
+++ Assets/Code/BallMovement.cs
@@ -7,12 +7,20 @@ public class BallMovement : MonoBehaviour
     public int PlayerId = 0;
 
     public float maxSpeed = 200f;
-    
-    void FixedUpdate()
+
+    private void FixedUpdate()
     {
         if (GetComponent<Rigidbody2D>().velocity.magnitude > maxSpeed)
         {
             GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().velocity.normalized * maxSpeed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Finish")
+        {
+            Time.timeScale = 0;
         }
     }
 }
