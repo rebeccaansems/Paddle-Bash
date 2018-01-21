bc5beea3fccb27af66f055c0d95ec159c62253d0
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-21 17:00:05 -0400

    Added art and some animation to join screen

---
+++ Assets/Code/Player Join/PlayerPanelData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPanelData : MonoBehaviour
+{
+
+    public Text PlayerName;
+    public int PlayerId, ColorNumber;
+
+    private void Start()
+    {
+        PlayerName.text = "EMPTY";
+    }
+
+    public void ChangeNameDisplayText()
+    {
+        PlayerName.text = "PLAYER 0" + PlayerId + 1;
+    }
+}
---
+++ Assets/Code/Player Join/RotateCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float RotateSpeed = 1.2f;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * RotateSpeed, Space.Self);
+    }
+}
