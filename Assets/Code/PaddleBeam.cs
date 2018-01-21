bbe703896e25679e95193ab52377f0120a3c4e49
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-21 19:47:14 -0400

    Added visuals of color changing

--- Assets/Code/PaddleBeam.cs
+++ Assets/Code/PaddleBeam.cs
@@ -8,9 +8,10 @@ public class PaddleBeam : MonoBehaviour
     public GameObject beamStartPrefab;
     public GameObject beamEndPrefab;
 
-    private GameObject beamStart;
-    private GameObject beamEnd;
-    private GameObject beam;
+    public Color[] Light;
+    public Gradient[] Laser, GlowBox;
+    public Material[] Beam;
+
     private LineRenderer line;
 
     private void Start()
@@ -22,5 +23,24 @@ public class PaddleBeam : MonoBehaviour
     private void Update()
     {
         line.SetPositions(new Vector3[] { beamStartPrefab.transform.position, beamEndPrefab.transform.position });
+    }
+
+    public void SetColor(int colorNum)
+    {
+        var startParticleSystem = beamStartPrefab.GetComponent<ParticleSystem>().colorOverLifetime;
+        var startGlowBox = beamStartPrefab.GetComponentsInChildren<ParticleSystem>()[1].colorOverLifetime;
+
+        beamStartPrefab.GetComponent<Light>().color = Light[colorNum];
+        startParticleSystem.color = Laser[colorNum];
+        startGlowBox.color = GlowBox[colorNum];
+
+        beamLineRendererPrefab.GetComponent<LineRenderer>().material = Beam[colorNum];
+
+        var endParticleSystem = beamEndPrefab.GetComponent<ParticleSystem>().colorOverLifetime;
+        var endGlowBox = beamEndPrefab.GetComponentsInChildren<ParticleSystem>()[1].colorOverLifetime;
+
+        beamEndPrefab.GetComponent<Light>().color = Light[colorNum];
+        endParticleSystem.color = Laser[colorNum];
+        endGlowBox.color = GlowBox[colorNum];
     }
 }
--- Assets/Code/Player Join/PlayerPanelData.cs
+++ Assets/Code/Player Join/PlayerPanelData.cs
@@ -7,15 +7,35 @@ public class PlayerPanelData : MonoBehaviour
 {
 
     public Text PlayerName;
+    public PaddleBeam PaddleBeam;
     public int PlayerId, ColorNumber;
+
+    private int prevColorNumber = 0;
 
     private void Start()
     {
         PlayerName.text = "EMPTY";
     }
 
+    private void Update()
+    {
+        if (prevColorNumber != ColorNumber)
+        {
+            prevColorNumber = ColorNumber;
+            UpdateColors();
+        }
+    }
+
     public void ChangeNameDisplayText()
     {
         PlayerName.text = "PLAYER 0" + PlayerId + 1;
+    }
+
+    public void UpdateColors()
+    {
+        if (PaddleBeam != null)
+        {
+            PaddleBeam.SetColor(ColorNumber);
+        }
     }
 }
