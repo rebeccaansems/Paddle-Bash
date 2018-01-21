6a43bf32c8b5dd78787104770dcefb42133c374e
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-21 02:54:09 -0400

    Changed bars/ballls to be glowing laser things

---
+++ Assets/Code/PaddleBeam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleBeam : MonoBehaviour
+{
+    public GameObject beamLineRendererPrefab;
+    public GameObject beamStartPrefab;
+    public GameObject beamEndPrefab;
+
+    private GameObject beamStart;
+    private GameObject beamEnd;
+    private GameObject beam;
+    private LineRenderer line;
+
+    private void Start()
+    {
+        line = beamLineRendererPrefab.GetComponent<LineRenderer>();
+        line.useWorldSpace = true;
+    }
+
+    private void Update()
+    {
+        line.SetPositions(new Vector3[] { beamStartPrefab.transform.position, beamEndPrefab.transform.position });
+    }
+}
