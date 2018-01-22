e08bc2fdae533be5e8a8934ac37251085a69433f
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-01-21 23:11:34 -0400

    Players join as correct color

--- Assets/Code/Player Join/PlayerPanelData.cs
+++ Assets/Code/Player Join/PlayerPanelData.cs
@@ -24,6 +24,7 @@ public class PlayerPanelData : MonoBehaviour
 
         PlayerName.text = "EMPTY";
         animator = GetComponent<Animator>();
+        UpdateColors();
     }
 
     private void Update()
